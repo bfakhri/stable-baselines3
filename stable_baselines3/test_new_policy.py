c308c89bb602ec4b4c4c0bd819d35f0b1dcef88c
Bijan Fakhri <bfakhri@asu.edu>
2025-04-19 11:06:08 -0500

    U

--- stable_baselines3/test_new_policy.py
+++ stable_baselines3/test_new_policy.py
@@ -3,8 +3,10 @@ import os
 import gymnasium as gym
 
 from stable_baselines3 import A2C
-from stable_baselines3.common.envs import make_atari_env
+from stable_baselines3.common.env_util import make_atari_env
 from stable_baselines3.common.vec_env import VecFrameStack
+import ale_py
+
 
 
 # Create Atari environment
